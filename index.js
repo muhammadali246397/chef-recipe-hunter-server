e9e8d1c2a495dc7edce8395bc975416b9e57810d
muhammadali246397 <muhammadali246397@gmail.com>
2023-05-02 18:57:44 +0600

    chef data complete and install cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require(cors)
 const port = 3000;
+app.use(cors());
 
 app.get('/',(req,res) => {
     res.send('this server is running');
