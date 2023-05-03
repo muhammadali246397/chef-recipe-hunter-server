eda6dd85b046ffdb1a594f8a7f53529272048cca
muhammadali246397 <muhammadali246397@gmail.com>
2023-05-03 11:01:11 +0600

    complete receipe data

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const cors = require(cors)
+const cors = require('cors')
+const chefs = require('./data/chef/chef.json')
 const port = 3000;
-app.use(cors());
+app.use(cors())
 
 app.get('/',(req,res) => {
     res.send('this server is running');
+})
+app.get('/chefs',(req,res) => {
+    res.send(chefs);
 })
 
 app.listen(port,() => {
